7e5b1eb49edc6c20594cec3816ee3429d9119ad6
Blaz Sajovic <blaz.sajovic@spletko.eu>
2013-10-14 11:18:11 +0200

    assets still

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,14 +23,6 @@
 
 $(function(){ $(document).foundation(); });
 
-$(document).ready(function() {
-    if ($("textarea").length > 0) {
-      var data = $("textarea");
-      $.each(data, function(i) {
-        CKEDITOR.replace(data[i].id);
-      });
-    }
-  });
 
 // --- funkcija za delay in izpise quotes
  
