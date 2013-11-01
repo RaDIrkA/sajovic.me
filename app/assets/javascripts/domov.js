88d4a6d5d290627598d6dbc568887cce06fa65cc
Blaz Sajovic <blaz.sajovic@spletko.eu>
2013-11-01 15:36:20 +0100

    commit
    few script

--- app/assets/javascripts/domov.js
+++ app/assets/javascripts/domov.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+// Function for blink 
+
+$(document).ready(function () { 
+	setInterval("$('.blink').fadeOut(150).fadeIn(150);",1500);
+});
